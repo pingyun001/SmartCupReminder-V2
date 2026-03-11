f04e40e1a4c53f0758a234608b56cf2be8f17b03
PYROG <2115471365@qq.com>
2026-03-12 00:47:31 +0800

    change tool name

---
+++ SOFTWARE/SIGNATURE_TOOLS/signature_prj/build/CMakeFiles/signature.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "D:/YQYPrivPrj/SmartCupReminder-V2/SOFTWARE/signature_tool/signature_exe/App/binSignature.c" "CMakeFiles/signature.dir/App/binSignature.c.obj" "gcc" "CMakeFiles/signature.dir/App/binSignature.c.obj.d"
+  "D:/YQYPrivPrj/SmartCupReminder-V2/SOFTWARE/signature_tool/signature_exe/App/winHal.c" "CMakeFiles/signature.dir/App/winHal.c.obj" "gcc" "CMakeFiles/signature.dir/App/winHal.c.obj.d"
+  "D:/YQYPrivPrj/SmartCupReminder-V2/SOFTWARE/signature_tool/signature_exe/main.c" "CMakeFiles/signature.dir/main.c.obj" "gcc" "CMakeFiles/signature.dir/main.c.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ SOFTWARE/SIGNATURE_TOOLS/signature_prj/build/CMakeFiles/signature.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/signature.dir/App/binSignature.c.obj"
+  "CMakeFiles/signature.dir/App/binSignature.c.obj.d"
+  "CMakeFiles/signature.dir/App/winHal.c.obj"
+  "CMakeFiles/signature.dir/App/winHal.c.obj.d"
+  "CMakeFiles/signature.dir/main.c.obj"
+  "CMakeFiles/signature.dir/main.c.obj.d"
+  "libsignature.dll.a"
+  "signature.exe"
+  "signature.exe.manifest"
+  "signature.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/signature.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
